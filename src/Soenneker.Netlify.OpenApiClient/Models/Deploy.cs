749cef67a8f698ab8c59ed83972e8ada8c6c7140
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-05-18 18:27:04 +0000

    Automated update

--- src/Soenneker.Netlify.OpenApiClient/Models/Deploy.cs
+++ src/Soenneker.Netlify.OpenApiClient/Models/Deploy.cs
@@ -112,6 +112,22 @@ namespace Soenneker.Netlify.OpenApiClient.Models
 #else
         public List<global::Soenneker.Netlify.OpenApiClient.Models.FunctionSchedule> FunctionSchedules { get; set; }
 #endif
+        /// <summary>The functions region for this deploy as an airport code.</summary>
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
+#nullable enable
+        public string? FunctionsRegion { get; set; }
+#nullable restore
+#else
+        public string FunctionsRegion { get; set; }
+#endif
+        /// <summary>Functions in the deploy that explicitly specify their own region(airport code).</summary>
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
+#nullable enable
+        public List<global::Soenneker.Netlify.OpenApiClient.Models.Deploy_functions_region_overrides>? FunctionsRegionOverrides { get; set; }
+#nullable restore
+#else
+        public List<global::Soenneker.Netlify.OpenApiClient.Models.Deploy_functions_region_overrides> FunctionsRegionOverrides { get; set; }
+#endif
         /// <summary>The id property</summary>
 #if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
 #nullable enable
@@ -276,6 +292,8 @@ namespace Soenneker.Netlify.OpenApiClient.Models
                 { "error_message", n => { ErrorMessage = n.GetStringValue(); } },
                 { "framework", n => { Framework = n.GetStringValue(); } },
                 { "function_schedules", n => { FunctionSchedules = n.GetCollectionOfObjectValues<global::Soenneker.Netlify.OpenApiClient.Models.FunctionSchedule>(global::Soenneker.Netlify.OpenApiClient.Models.FunctionSchedule.CreateFromDiscriminatorValue)?.AsList(); } },
+                { "functions_region", n => { FunctionsRegion = n.GetStringValue(); } },
+                { "functions_region_overrides", n => { FunctionsRegionOverrides = n.GetCollectionOfObjectValues<global::Soenneker.Netlify.OpenApiClient.Models.Deploy_functions_region_overrides>(global::Soenneker.Netlify.OpenApiClient.Models.Deploy_functions_region_overrides.CreateFromDiscriminatorValue)?.AsList(); } },
                 { "id", n => { Id = n.GetStringValue(); } },
                 { "locked", n => { Locked = n.GetBoolValue(); } },
                 { "name", n => { Name = n.GetStringValue(); } },
@@ -316,6 +334,8 @@ namespace Soenneker.Netlify.OpenApiClient.Models
             writer.WriteStringValue("error_message", ErrorMessage);
             writer.WriteStringValue("framework", Framework);
             writer.WriteCollectionOfObjectValues<global::Soenneker.Netlify.OpenApiClient.Models.FunctionSchedule>("function_schedules", FunctionSchedules);
+            writer.WriteStringValue("functions_region", FunctionsRegion);
+            writer.WriteCollectionOfObjectValues<global::Soenneker.Netlify.OpenApiClient.Models.Deploy_functions_region_overrides>("functions_region_overrides", FunctionsRegionOverrides);
             writer.WriteStringValue("id", Id);
             writer.WriteBoolValue("locked", Locked);
             writer.WriteStringValue("name", Name);
--- src/Soenneker.Netlify.OpenApiClient/Models/Deploy_functions_region_overrides.cs
+++ src/Soenneker.Netlify.OpenApiClient/Models/Deploy_functions_region_overrides.cs
@@ -9,19 +9,11 @@ namespace Soenneker.Netlify.OpenApiClient.Models
 {
     [global::System.CodeDom.Compiler.GeneratedCode("Kiota", "1.0.0")]
     #pragma warning disable CS1591
-    public partial class Function : IAdditionalDataHolder, IParsable
+    public partial class Deploy_functions_region_overrides : IAdditionalDataHolder, IParsable
     #pragma warning restore CS1591
     {
         /// <summary>Stores additional data not described in the OpenAPI description found when deserializing. Can be used for serialization as well.</summary>
         public IDictionary<string, object> AdditionalData { get; set; }
-        /// <summary>The id property</summary>
-#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
-#nullable enable
-        public string? Id { get; set; }
-#nullable restore
-#else
-        public string Id { get; set; }
-#endif
         /// <summary>The name property</summary>
 #if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
 #nullable enable
@@ -38,30 +30,22 @@ namespace Soenneker.Netlify.OpenApiClient.Models
 #else
         public string Region { get; set; }
 #endif
-        /// <summary>The sha property</summary>
-#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
-#nullable enable
-        public string? Sha { get; set; }
-#nullable restore
-#else
-        public string Sha { get; set; }
-#endif
         /// <summary>
-        /// Instantiates a new <see cref="global::Soenneker.Netlify.OpenApiClient.Models.Function"/> and sets the default values.
+        /// Instantiates a new <see cref="global::Soenneker.Netlify.OpenApiClient.Models.Deploy_functions_region_overrides"/> and sets the default values.
         /// </summary>
-        public Function()
+        public Deploy_functions_region_overrides()
         {
             AdditionalData = new Dictionary<string, object>();
         }
         /// <summary>
         /// Creates a new instance of the appropriate class based on discriminator value
         /// </summary>
-        /// <returns>A <see cref="global::Soenneker.Netlify.OpenApiClient.Models.Function"/></returns>
+        /// <returns>A <see cref="global::Soenneker.Netlify.OpenApiClient.Models.Deploy_functions_region_overrides"/></returns>
         /// <param name="parseNode">The parse node to use to read the discriminator value and create the object</param>
-        public static global::Soenneker.Netlify.OpenApiClient.Models.Function CreateFromDiscriminatorValue(IParseNode parseNode)
+        public static global::Soenneker.Netlify.OpenApiClient.Models.Deploy_functions_region_overrides CreateFromDiscriminatorValue(IParseNode parseNode)
         {
             if(ReferenceEquals(parseNode, null)) throw new ArgumentNullException(nameof(parseNode));
-            return new global::Soenneker.Netlify.OpenApiClient.Models.Function();
+            return new global::Soenneker.Netlify.OpenApiClient.Models.Deploy_functions_region_overrides();
         }
         /// <summary>
         /// The deserialization information for the current model
@@ -71,10 +55,8 @@ namespace Soenneker.Netlify.OpenApiClient.Models
         {
             return new Dictionary<string, Action<IParseNode>>
             {
-                { "id", n => { Id = n.GetStringValue(); } },
                 { "name", n => { Name = n.GetStringValue(); } },
                 { "region", n => { Region = n.GetStringValue(); } },
-                { "sha", n => { Sha = n.GetStringValue(); } },
             };
         }
         /// <summary>
@@ -84,10 +66,8 @@ namespace Soenneker.Netlify.OpenApiClient.Models
         public virtual void Serialize(ISerializationWriter writer)
         {
             if(ReferenceEquals(writer, null)) throw new ArgumentNullException(nameof(writer));
-            writer.WriteStringValue("id", Id);
             writer.WriteStringValue("name", Name);
             writer.WriteStringValue("region", Region);
-            writer.WriteStringValue("sha", Sha);
             writer.WriteAdditionalData(AdditionalData);
         }
     }
